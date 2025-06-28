af47a866c9e44db27a1a8e63c0244773f1c4ec03
nganthuy0111 <nttngan0111@gmail.com>
2025-06-28 10:40:07 +0700

    Update database connection string, add CORS policy for frontend, and include Entity Framework Core design package

--- AIChatbot_BE/Web_API/Program.cs
+++ AIChatbot_BE/Web_API/Program.cs
@@ -21,6 +21,15 @@ builder.Services.AddDbContext<Repositories.DBContext.AichatbotDbContext>(options
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        policy => policy
+            .WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+    );
+});
 
 var app = builder.Build();
 
@@ -64,7 +73,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors("AllowFrontend");
 app.UseAuthorization();
 
 app.MapControllers();
