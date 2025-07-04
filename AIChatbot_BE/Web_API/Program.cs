62f4ee2e5331a58acd2862fd63ed5dc87ba30973
daqkienninh <142190951+daqkienninh@users.noreply.github.com>
2025-07-04 22:55:07 +0700

    add cros fe

--- AIChatbot_BE/Web_API/Program.cs
+++ AIChatbot_BE/Web_API/Program.cs
@@ -64,7 +64,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend",
         policy => policy
-            .WithOrigins("http://localhost:5173")
+            .WithOrigins("ai-chatbot-fe-web.vercel.app")
             .AllowAnyHeader()
             .AllowAnyMethod()
     );
